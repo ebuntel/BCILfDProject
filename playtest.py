68917fbe67feeb4279e28d730ea96760c6369979
Ellery Buntel <buntel.ellery@gmail.com>
2022-04-21 12:31:30 -0400

    Added callback to playtest

--- playtest.py
+++ playtest.py
@@ -96,4 +96,9 @@ eval_py_env = suite_gym.load(env_name,
 train_env = tf_py_environment.TFPyEnvironment(train_py_env)
 eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
-play.play(env, keys_to_action = adict, zoom = 4)
+def callback(obs_t, obs_tp1, action, rew):
+    return [rew, obs_t, action]
+
+plotter = play.PlayPlot(callback, 30 * 5, ["reward"])
+
+play.play(env, keys_to_action = adict, zoom = 4, fps = 15, callback = plotter.callback)
