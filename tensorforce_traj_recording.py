fcaaee2b3e86fd4d3bca6aa177baa57e130c13a6
Ellery Buntel <buntel.ellery@gmail.com>
2022-06-23 14:55:28 -0400

    Added trajectory to tensorforce trace converter

--- tensorforce_traj_recording.py
+++ tensorforce_traj_recording.py
@@ -5,7 +5,7 @@ import os
 
 from datetime import datetime
 from itertools import count
-from tensorforce import Environment
+from tensorforce import Environment, Agent
 from gym.utils import play
 
 import ale_py.roms as roms
@@ -16,7 +16,7 @@ import numpy as np
 
 
 def main():
-    display = pyvirtualdisplay.Display(visible=1, size=(1400, 900)).start()
+    #display = pyvirtualdisplay.Display(visible=1, size=(1400, 900)).start()
 
     env_name = 'ALE/MsPacman-v5'
 
@@ -32,13 +32,13 @@ def main():
         scale_obs = False)
     env = gym.wrappers.FrameStack(env, num_stack = 4)
 
-    write_custom_recording_file(in_directory='', out_directory='pacman_traces', env=env)
+    write_custom_recording_file(in_directory='trajectory06-13-2022,13-15.pkl', out_directory='pacman_traces', env=env)
 
     # Pretrain a new agent on the recorded traces: for 30 iterations, feed the
     # experience of one episode to the agent and subsequently perform one update
-    # environment = Environment.create(environment=env)
-    # agent = Agent.create(agent='benchmarks/configs/ppo.json', environment=environment)
-    # agent.pretrain(directory='ppo-traces', num_iterations=30, num_traces=1, num_updates=1)
+    environment = Environment.create(environment=env)
+    agent = Agent.create(agent='dqn', environment=env)
+    agent.pretrain(directory='pacman_traces', num_iterations=30, num_traces=1, num_updates=1)
 
     # # Evaluate the pretrained agent
     # runner = Runner(agent=agent, environment=environment)
@@ -46,8 +46,8 @@ def main():
     # runner.close()
 
     # # Close agent and environment
-    # agent.close()
-    # environment.close()
+    agent.close()
+    environment.close()
 
 def write_custom_recording_file(in_directory, out_directory, env):
     # Start recording traces after 80 episodes -- by then, the environment is solved
@@ -58,37 +58,46 @@ def write_custom_recording_file(in_directory, out_directory, env):
     
     actions = []
 
-    for trajectory in trajectories:
-        actions.append(trajectories[1].action)
+    for i in range(len(trajectories)):
+        print(trajectories[i].action)
+        actions.append(trajectories[i][0])
+
+    #print(actions)
 
     # Record 20 episodes
-    for episode in range(20):
 
-        # Record episode experience
-        episode_states = list()
-        episode_actions = list()
-        episode_terminal = list()
-        episode_reward = list()
+    # Record episode experience
+    episode_states = list()
+    episode_actions = list()
+    episode_terminal = list()
+    episode_reward = list()
 
-        # Evaluation episode
-        states = environment.reset()
-        terminal = False
-        while not terminal:
-            episode_states.append(states)
-            action = actions.pop(0)
-            episode_actions.append(action)
-            states, terminal, reward = environment.execute(actions=action)
-            episode_terminal.append(terminal)
-            episode_reward.append(reward)
+    # Evaluation episode
+    states = environment.reset()
+    terminal = False
+    while not terminal and len(actions) >= 1:
+        episode_states.append(states)
+        action = actions.pop(0)
+        episode_actions.append(action)
+        states, terminal, reward = environment.execute(actions=action)
+        if(len(actions) == 0):
+            terminal = True
+        episode_terminal.append(terminal)
+        episode_reward.append(reward)
 
-        # Write recorded episode trace to npz file
-        np.savez_compressed(
-            file=os.path.join(out_directory, 'trace-{:09d}.npz'.format(episode)),
-            states=np.stack(episode_states, axis=0),
-            actions=np.stack(episode_actions, axis=0),
-            terminal=np.stack(episode_terminal, axis=0),
-            reward=np.stack(episode_reward, axis=0)
-        )
+    print("# of states: ", len(episode_states))
+    print("# of actions: ", len(episode_actions))
+    print("# of terminal bools: ", len(episode_terminal))
+    print("# of rewards: ", len(episode_reward))
+
+    # Write recorded episode trace to npz file
+    np.savez_compressed(
+        file=os.path.join(out_directory, 'trace-{:09d}.npz'.format(1)),
+        states=np.stack(episode_states, axis=0),
+        actions=np.stack(episode_actions, axis=0),
+        terminal=np.stack(episode_terminal, axis=0),
+        reward=np.stack(episode_reward, axis=0)
+    )
 
 if __name__ ==  '__main__':
     main()
